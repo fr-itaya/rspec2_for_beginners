870118162a31d8a6dc75b1d786d0665aca2e80fd
fr-itaya <itaya@fact-real.com>
2015-09-11 01:33:20 +0000

    deleted strings

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -4,10 +4,6 @@ describe MessageFilter do
   before do
     @filter = MessageFilter.new('foo')
   end
-  it 'should detect message with NG word' do
-    @filter.should be_detect('hello from foo')
-  end
-  it 'should not detect message without NG word' do
-    @filter.should_not be_detect('hello world!')
-  end
+  it { @filter.should be_detect('hello from foo') }
+  it { @filter.should_not be_detect('hello world!') }
 end
