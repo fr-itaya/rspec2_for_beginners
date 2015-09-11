00f07f1929ad47beba5535ad6e8849f6ee6d003e
fr-itaya <itaya@fact-real.com>
2015-09-11 01:21:55 +0000

    deleted (:each) from before method

--- message_filter_spec.rb
+++ message_filter_spec.rb
@@ -1,7 +1,7 @@
 require_relative 'message_filter'
 
 describe MessageFilter do
-  before (:each) do
+  before do
     @filter = MessageFilter.new('foo')
   end
   it 'should detect message with NG word' do
